522b35fdbec0e9d729a9f587c1bf264ce8059b16
James Sofra <james.sofra@gmail.com>
2016-09-18 14:23:17 +1000

    Add data readers for the new graphs and charts.

--- src/data_readers.clj
+++ src/data_readers.clj
@@ -1,9 +1,28 @@
-{ds/b data-scope.charts/scope-bar
- ds/l data-scope.charts/scope-line
- ds/a data-scope.charts/scope-area
- ds/s data-scope.charts/scope-stacked-area
- ds/p data-scope.charts/scope-pie
+{ds/b  data-scope.charts/scope-bar
+ ds/l  data-scope.charts/scope-line
+ ds/a  data-scope.charts/scope-area
+ ds/sa data-scope.charts/scope-stacked-area
+ ds/p  data-scope.charts/scope-pie
 
- ds/g data-scope.graphs/scope-graph
- ds/t data-scope.graphs/scope-tree
- ds/d data-scope.graphs/scope-dot}
+ ds/b-sum  data-scope.charts/scope-bar-sum
+ ds/l-sum  data-scope.charts/scope-line-sum
+ ds/a-sum  data-scope.charts/scope-area-sum
+ ds/sa-sum data-scope.charts/scope-stacked-area-sum
+ ds/p-sum  data-scope.charts/scope-pie-sum
+
+ ds/b-max  data-scope.charts/scope-bar-max
+ ds/l-max  data-scope.charts/scope-line-max
+ ds/a-max  data-scope.charts/scope-area-max
+ ds/sa-max data-scope.charts/scope-stacked-area-max
+ ds/p-max  data-scope.charts/scope-pie-max
+
+ ds/b-min  data-scope.charts/scope-bar-min
+ ds/l-min  data-scope.charts/scope-line-min
+ ds/a-min  data-scope.charts/scope-area-min
+ ds/sa-min data-scope.charts/scope-stacked-area-min
+ ds/p-min  data-scope.charts/scope-pie-min
+
+ ds/g    data-scope.graphs/scope-graph
+ ds/t    data-scope.graphs/scope-tree
+ ds/trie data-scope.graphs/scope-tree
+ ds/d    data-scope.graphs/scope-dot}
