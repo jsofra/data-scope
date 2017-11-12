f8cd3571e062731b5ab52bd93ca0710b60922cd4
James Sofra <james.sofra@gmail.com>
2017-11-13 09:15:43 +1100

    Add the ability to adjust the font size of the inspect-tree.

--- src/data_scope/inspect.clj
+++ src/data_scope/inspect.clj
@@ -10,8 +10,22 @@
                                data))
        inspector/inspect-table))
 
+(defn inspect-tree
+  "A version of inspect-tree that allow control of the font size."
+  [data font-size]
+  (let [tree (javax.swing.JTree. (inspector/tree-model data))]
+    (doto (javax.swing.JFrame. "Clojure Inspector")
+      (.add (javax.swing.JScrollPane.
+             (doto tree
+               (.setRowHeight (+ font-size 4))
+               (.setFont (.deriveFont (.getFont tree) (float font-size))))))
+      (.pack)
+      (.setVisible true))))
+
+(def ds-i-font-size (atom 18))
+
 (defn scope-inspect-tree [form]
-  `(let [form# ~form] ('~inspector/inspect-tree form#) form#))
+  `(let [form# ~form] (~inspect-tree form# @ds-i-font-size) form#))
 
 (defn scope-inspect-tree-thread-last [form]
   `((fn [x#]
