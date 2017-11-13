9bfefb52e5187f716606ca0efd189a7622b56cf3
James Sofra <james.sofra@gmail.com>
2017-11-13 12:23:16 +1100

    Add buttons to controll font size to inspect-tree.

--- src/data_scope/inspect.clj
+++ src/data_scope/inspect.clj
@@ -10,22 +10,49 @@
                                data))
        inspector/inspect-table))
 
-(defn inspect-tree
-  "A version of inspect-tree that allow control of the font size."
-  [data font-size]
-  (let [tree (javax.swing.JTree. (inspector/tree-model data))]
-    (doto (javax.swing.JFrame. "Clojure Inspector")
-      (.add (javax.swing.JScrollPane.
-             (doto tree
-               (.setRowHeight (+ font-size 4))
-               (.setFont (.deriveFont (.getFont tree) (float font-size))))))
-      (.pack)
-      (.setVisible true))))
-
 (def ds-i-font-size (atom 18))
 
+(defn inspect-tree
+  "A version of inspect-tree that allow control of the font size."
+  ([data]
+   (inspect-tree data @ds-i-font-size))
+  ([data font-size]
+   (let [frame         (javax.swing.JFrame. "Clojure Inspector")
+         tree          (javax.swing.JTree. (inspector/tree-model data))
+         reset-tree!   (fn [font font-size]
+                         (doto tree
+                           (.setRowHeight (+ font-size 10))
+                           (.setFont (.deriveFont font (float font-size)))))
+         font-listener (proxy [javax.swing.AbstractAction] []
+                         (actionPerformed [event]
+                           (let [font      (.getFont tree)
+                                 font-size (case (.getActionCommand event)
+                                             "+" (float (+ (.getSize font) 2))
+                                             "-" (float (- (.getSize font) 2)))]
+                             (reset! ds-i-font-size font-size)
+                             (reset-tree! font font-size)
+                             (.pack frame))))
+         font+         (doto (javax.swing.JButton. "+")
+                         (.addActionListener font-listener))
+         font-         (doto (javax.swing.JButton. "-")
+                         (.addActionListener font-listener))
+         top-panel     (javax.swing.JPanel. (java.awt.FlowLayout.
+                                             java.awt.FlowLayout/LEADING))
+         panel         (javax.swing.JPanel. (java.awt.BorderLayout.))]
+     (doto frame
+       (.add (doto panel
+               (.add (doto top-panel
+                       (.add font+)
+                       (.add font-))
+                     java.awt.BorderLayout/PAGE_START)
+               (.add (javax.swing.JScrollPane.
+                      (reset-tree! (.getFont tree) font-size))
+                     java.awt.BorderLayout/CENTER)))
+       (.pack)
+       (.setVisible true)))))
+
 (defn scope-inspect-tree [form]
-  `(let [form# ~form] (~inspect-tree form# @ds-i-font-size) form#))
+  `(let [form# ~form] (~inspect-tree form#) form#))
 
 (defn scope-inspect-tree-thread-last [form]
   `((fn [x#]
